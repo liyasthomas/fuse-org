a172a861f50f2c71a3c7c16530f3094920490a9f
liyasthomas <liyascthomas@gmail.com>
2018-05-24 03:57:40 +0530

    Clean up

--- service-worker.js
+++ service-worker.js
@@ -6,30 +6,29 @@ var urlsToCache = [
 //  '/src/my-app.html',
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function (event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function (cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
 });
 
 self.addEventListener('activate', event => {
- event.waitUntil(self.clients.claim());
+	event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function (response) {
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
 });
